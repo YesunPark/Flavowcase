9010bdd853b6949e6f4beb6a65667d2ec536a000
YesunPark <lynn08082@gmail.com>
2024-05-10 12:42:13 +0900

    init

---
+++ src/main/java/com/flavowcase/FlavowcaseApplication.java
@@ -0,0 +1,13 @@
+package com.flavowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlavowcaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlavowcaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flavowcase/FlavowcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flavowcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlavowcaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
